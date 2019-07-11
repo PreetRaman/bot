8079379775130d3eabe18ad953e15566387b17db
preet <ramanpreet.kaur@sinovo.de>
2019-07-11 17:47:09 +0200

    Add base package for slack bot

--- src/main/java/com/bot/slackBot/SlackBotApplication.java
+++ src/main/java/com/bot/slackBot/SlackBotApplication.java
@@ -2,11 +2,17 @@ package com.bot.slackBot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"me.ramswaroop.jbot", "com.bot.slackBot"})
 public class SlackBotApplication {
 
-	public static void main(String[] args) {
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+    public static void main(String[] args) {
 		SpringApplication.run(SlackBotApplication.class, args);
 	}
 
